76c34a683d92e3dbdc5bef7ab14ef13d5611083b
Kacper <kacpergierycz@gmail.com>
2021-06-16 15:55:58 +0200

    xml bean configuration and main use of class

--- AOP/src/main/java/aspects/AopMain.java
+++ AOP/src/main/java/aspects/AopMain.java
@@ -3,11 +3,13 @@ package aspects;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import aspects.service.ShapeService;
+
 public class AopMain {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-
+		ShapeService shapeServiece = ctx.getBean("shapeService", ShapeService.class);
 		
 	}
 
