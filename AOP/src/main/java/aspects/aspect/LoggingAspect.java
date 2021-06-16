d50ddd3ca5e29fd085a07518ccfd4f1460c6de2d
Kacper <kacpergierycz@gmail.com>
2021-06-16 18:25:50 +0200

    @Poitcut(* * name(..)

--- AOP/src/main/java/aspects/aspect/LoggingAspect.java
+++ AOP/src/main/java/aspects/aspect/LoggingAspect.java
@@ -2,15 +2,25 @@ package aspects.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
 //	@Before("execution(public String aspects.model.Circle.getName())")
-	@Before("execution(public * *.get*())")
+	@Before("AllGetters()")
 	public void LoggingAdvice() {
 		
 		System.out.println("Advice run .Get method caled");
 	}
+	
+	@Before("execution(* get*(..))")
+	public void secondAdvice() {
+		
+		System.out.println("second Advice ");
+	}
+	
+	@Pointcut("execution(* get*(..))")
+	public void AllGetters() {}
 	
 }
