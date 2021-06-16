ad6b6d2b29868e15d02605b0c72f554ad0a5144a
Kacper <kacpergierycz@gmail.com>
2021-06-16 15:48:45 +0200

    shape classes

---
+++ AOP/src/main/java/aspects/service/ShapeService.java
@@ -0,0 +1,27 @@
+package aspects.service;
+
+import aspects.model.Circle;
+import aspects.model.Triangle;
+
+public class ShapeService {
+	
+	private Circle circle;
+	private Triangle triangle;
+	
+	
+	public Circle getCircle() {
+		return circle;
+	}
+	public void setCircle(Circle circle) {
+		this.circle = circle;
+	}
+	public Triangle getTriangle() {
+		return triangle;
+	}
+	public void setTriangle(Triangle triangle) {
+		this.triangle = triangle;
+	}
+	
+	
+
+}
