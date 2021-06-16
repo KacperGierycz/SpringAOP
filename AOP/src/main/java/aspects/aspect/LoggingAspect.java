8fc2f843d854ce305278166d9ac43bf58af7dfde
Kacper <kacpergierycz@gmail.com>
2021-06-16 18:12:08 +0200

    Wild cards

--- AOP/src/main/java/aspects/aspect/LoggingAspect.java
+++ AOP/src/main/java/aspects/aspect/LoggingAspect.java
@@ -6,7 +6,8 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
 
-	@Before("execution(public String aspects.model.Circle.getName())")
+//	@Before("execution(public String aspects.model.Circle.getName())")
+	@Before("execution(public * *.get*())")
 	public void LoggingAdvice() {
 		
 		System.out.println("Advice run .Get method caled");
